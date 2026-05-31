2e13d3b47ffed1b251ff58d9c489bd6c91de21b4
Makers-vip-tools <bankeylal7897665273@gmail.com>
2026-05-31 14:38:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Aapke Bot ka token (BotFather se le kar yahan daalein)
+BOT_TOKEN = "8810164249:AAGRGeKiCt9q7Vvlfm1bAWIfgXjlvLDROzo"
+
+# Aapka Firebase URL path ke sath (datapackgive)
+FIREBASE_URL = "https://earning-a9b0c-default-rtdb.firebaseio.com/datapackgive"
+
